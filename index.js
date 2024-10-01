bc697807a4957bfd34cc201cd3cc4b4ae3a9598c
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-09-30 19:59:06 -0700

    fixed calendar removal

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const CALENDAR_MINIMUM_WIDTH = 850; // I found this number by shrinking the page until the calendar seemed too dificult to read
 
 // runs when the page is done loading
-// this means loading all elements and whatnot
+// calls on the on_resize function for loading page
 window.addEventListener('load', function () {
+  let current_screen_width = window.innerWidth
 
   // if the windows width is less then the minimum calender width,
   // remove the calendar and display alternate text that the page is too small
   // and redirect them to the calendar externally 
-  if (window.innerWidth <= CALENDAR_MINIMUM_WIDTH) {
+  if (current_screen_width <= CALENDAR_MINIMUM_WIDTH) {
     document.getElementById("google calendar").remove()
-    document.getElementById("alt calendar text").classList.toggle("visible");
+    document.getElementById("alt calendar text").classList.toggle("hidden")
   } 
 })  
