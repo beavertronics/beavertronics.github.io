c3e2dc3ca1164a8f198cf8bcbb8d2306447bf790
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2026-09-25 20:57:48 +0000

    x

--- credits/credits.js
+++ credits/credits.js
@@ -1,15 +1,33 @@
 async function loadContributors() {
-    const contributors = (await (await fetch('https://static.team5970.org/root/data/contributors.json')).json())
-    console.log(contributors)
+    // load the known information (initials, year of graduation, etc) from the JSON file and github
+    const jsonContributors = (await (await fetch('https://static.team5970.org/root/data/contributors.json')).json())
+    try {
+        const githubContributors = (await (await fetch('https://api.github.com/repos/beavertronics/web-assets/contributors')).json())
+    } catch (error) {
+        alert(error)
+    }
+
+    // create grid for all contributors
     const grid = $("#contributor-grid")
 
-    for (const contributorUsername of Object.keys(contributors)) {
-        const contributor = contributors[contributorUsername]
-        const response = await fetch(`https://api.github.com/users/${contributorUsername}`)
+    // go through each github contributor
+    for (const githubContributor of githubContributors) {
+        // use the info in the JSON if it exists, otherwise make a template
+        if (githubContributor['login'] in Object.keys(jsonContributors)) {
+            const contributorInfo = jsonContributors[githubContributor]
+        }
+        else {
+            const contributorInfo = {
+                'initials': 'N/A',
+                'gradYear': 'N/A'
+            }
+        }
+
+        const response = await fetch(githubContributor['url'])
         const data = await response.json()
 
         const cell = $(`
-            <a href="https://github.com/${contributorUsername}" target="_blank" class="contributor-cell">
+            <a href="${githubContributor['html_url']}" target="_blank" class="contributor-cell">
                 <img src="${data.avatar_url}" alt="${contributor['initials']}">
                 <p class="contributor-initials">${contributor['initials']}</p>
                 <p class="contributor-grad-year">Class of ${contributor['gradYear']}</p>
